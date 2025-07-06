81ebe1834a71371d7e887acfbc5bef610dfda4e3
YALLICO OWNER <yallico2024@gmail.com>
2025-07-06 10:27:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,22 +17,14 @@ global.botNumber = '' //Ejemplo: 573218138672
 
 global.owner = [
 // <-- Número @s.whatsapp.net -->
-  ['51969214380', '🎄 Propietario', true],
-  ['51994114690', 'creador 2 🌛', true],
-  ['51988013368', 'creador 3 ☄️', true],
-  ['51919199620'],
-  ['527721892735'],
-  
-// <-- Número @lid -->
-  ['193196806148194', '🎄 Propietario', true],
-  ['31096184029342', 'creador 2 🌛', true],
-  ['265322175185097', 'creador 3 ☄️', true]
+  ['51936994155', '🎄 Propietario', true],
+  ['51957764960', 'creador 2 🌛', true]
 ];
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.mods = []
-global.suittag = ['51969214380'] 
+global.suittag = ['51936994155'] 
 global.prems = []
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
@@ -40,47 +32,47 @@ global.prems = []
 global.libreria = 'Baileys'
 global.baileys = 'V 6.7.17' 
 global.vs = '2.2.0'
-global.nameqr = 'Itoshi V2'
-global.namebot = 'rin itoshi V2 🎄'
+global.nameqr = 'Eazzy V2'
+global.namebot = 'Eazzy X Bot'
 global.sessions = 'Sessions'
 global.jadi = 'JadiBots' 
 global.blackJadibts = true
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = '🎄  𝒓𝒊𝒏 𝒊𝒕𝒐𝒔𝒉𝒊 𝒃𝒐𝒕 𝑴𝑫  🎋'
-global.botname = '✦⃟⛧ _𝑨𝑹𝑰𝑵 𝑰𝑻𝑶𝑺𝑯𝑰⛧ 𝑩𝑶𝑻 𝑽²_ 🎄┋⃟✧'
-global.wm = '◟RIN ITOSHI Ｖ2◞'
-global.author = 'mᥲძᥱ ᑲᥡ : 🌈ᵀ͢ᴴᴱ𝄟⏤͟͟͞͞⃝Black'
-global.dev = '୧ㅤミ★ 》 Tʜᴇ Bʟᴀᴄᴋ 𝘰𝘧𝘤《★彡 🎋'
-global.bot = '𝑹𝒊𝒏 𝑰𝒕𝒐𝒔𝒉𝒊'
-global.textbot = 'ℝ𝕀ℕ 𝕀𝕋𝕆ℙ𝕊ℍ𝕀 𝕍2 𝕏 𝔹𝕃𝔸ℂ𝕂'
-global.etiqueta = '@ʙʟᴀᴄᴋ.ᴏғᴄ'
+global.packname = 'ᴇᴀᴢᴢʏ ꜱᴛɪᴄᴋᴇʀ 🤍'
+global.botname = 'ᴇᴀᴢᴢʏ ᴀᴄᴛᴜᴀʟɪᴢᴀᴅᴏ ᴠ2 🤍'
+global.wm = 'ᴇᴀᴢᴢʏ x ʙᴏᴛ 🤍'
+global.author = 'ᴇᴀᴢᴢʏ x ʙᴏᴛ 🤍'
+global.dev = 'ᴇᴀᴢᴢʏ x ʙᴏᴛ 🤍'
+global.bot = 'ᴇᴀᴢᴢʏ x ʙᴏᴛ 🤍'
+global.textbot = 'ᴇᴀᴢᴢʏ x ʙᴏᴛ 🤍'
+global.etiqueta = 'ᴇᴀᴢᴢʏ x ʙᴏᴛ 🤍'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.moneda = 'coins'
-global.welcom1 = '⚽ 𝐄ძі𝗍ᥲ ᥱᥣ ᥕᥱᥣᥴ᥆mᥱ ᥴ᥆ᥒ #sᥱ𝗍ᥕᥱᥣᥴ᥆mᥱ'
-global.welcom2 = '⚽ 𝐄ძі𝗍ᥲ ᥱᥣ ᥕᥱᥣᥴ᥆mᥱ ᥴ᥆ᥒ #sᥱ𝗍ᑲᥡᥱ'
-global.banner = 'https://files.catbox.moe/8u4f6p.png'
-global.avatar = 'https://files.catbox.moe/jiarb6.jpg'
-global.logo = 'https://files.catbox.moe/0iehy3.jpg'
+global.welcom1 = 'ᴇᴅɪᴛᴀ ᴇʟ ᴡᴇʟᴄᴏᴍᴇ ᴄᴏɴ .ꜱᴇᴛᴡᴇʟᴄᴏᴍᴇ 🤍'
+global.welcom2 = 'ᴇᴅɪᴛᴀ ʟᴀ ᴅᴇꜱᴘᴇᴅɪᴅᴀ ᴄᴏɴ .ꜱᴇᴛʙʏᴇ 🤍'
+global.banner = 'https://qu.ax/cqUYc.jpg'
+global.avatar = 'https://qu.ax/cqUYc.jpg'
+global.logo = 'https://qu.ax/cqUYc.jpg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.gp1 = 'https://whatsapp.com/channel/0029VbAtbPA84OmJSLiHis2U'
-global.comunidad1 = 'https://whatsapp.com/channel/0029VbAtbPA84OmJSLiHis2U'
-global.channel = 'https://whatsapp.com/channel/0029VbAtbPA84OmJSLiHis2U'
-global.channel2 = 'https://whatsapp.com/channel/0029VbAtbPA84OmJSLiHis2U'
-global.md = 'https://github.com/the-27/Rin-Itoshi-Bot-V2'
-global.correo = 'blackoficial2025@gmail.com'
+global.gp1 = 'https://chat.whatsapp.com/IFkJ3LVePPC0FDGFuDul98'
+global.comunidad1 = 'https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m'
+global.channel = 'https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m'
+global.channel2 = 'https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m'
+global.md = 'https://github.com'
+global.correo = 'yallico20224@gmail.com'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363401008003732@newsletter',
+ch1: '120363419947391620@newsletter',
 }
 global.multiplier = 60
 
